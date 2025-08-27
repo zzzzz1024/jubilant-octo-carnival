f7a477dfeeb682f414b98df57ff2b001f3cdd25c
StageDog <aksanajw845@gmail.com>
2025-08-28 00:48:30 +0800

    style: 调整界面示例的大小写

--- src/界面示例/渲染vue界面.ts
+++ src/界面示例/渲染vue界面.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import app from './app.vue';
 
 $(() => {
-  const app = createApp(App);
-  app.mount('#app');
+  createApp(app).mount('#app');
 });
