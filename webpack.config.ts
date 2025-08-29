7357f2e1b9bc72ab76cba8e298d75ab68a05a871
StageDog <aksanajw845@gmail.com>
2025-08-29 18:55:40 +0800

    feat: 移除 `@` 作为路径的功能

--- webpack.config.ts
+++ webpack.config.ts
@@ -246,7 +246,6 @@ function parse_configuration(entry: Entry): (_env: any, argv: any) => webpack.Co
           request.startsWith('-') ||
           request.startsWith('.') ||
           request.startsWith('/') ||
-          request.startsWith('@/') ||
           request.startsWith('http') ||
           path.isAbsolute(request) ||
           fs.existsSync(path.join(context, request)) ||
