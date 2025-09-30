d2269bba7bf94c34146f397166c8fba9a7bb384a
StageDog <aksanajw845@gmail.com>
2025-09-30 13:18:45 +0800

    feat: 单独刷新脚本或前端界面

--- webpack.config.ts
+++ webpack.config.ts
@@ -99,6 +99,11 @@ function watch_tavern_helper(compiler: webpack.Compiler) {
     compiler.hooks.done.tap('watch_tavern_helper', () => {
       console.info('\n\x1b[36m[tavern_helper]\x1b[0m 检测到完成编译, 推送更新事件...');
       io.emit('iframe_updated');
+      if (compiler.options.plugins.find(plugin => plugin instanceof HtmlWebpackPlugin)) {
+        io.emit('message_iframe_updated');
+      } else {
+        io.emit('script_iframe_updated');
+      }
     });
   }
 }
