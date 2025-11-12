e4b2591dc34204b9502b04d8e761a6b9bfe82040
StageDog <aksanajw845@gmail.com>
2025-11-12 18:32:18 +0800

    feat: 强调使用 pinia、zod

--- src/脚本示例/settings.ts
+++ src/脚本示例/settings.ts
@@ -7,14 +7,9 @@ const Settings = z
 export const useSettingsStore = defineStore('settings', () => {
   const settings = ref(Settings.parse(getVariables({ type: 'script', script_id: getScriptId() })));
 
-  watch(
-    settings,
-    new_settings => {
-      insertOrAssignVariables(klona(new_settings), { type: 'script', script_id: getScriptId() });
-    },
-    { immediate: true, deep: true },
-  );
-  return {
-    settings,
-  };
+  watchEffect(() => {
+    insertOrAssignVariables(klona(settings.value), { type: 'script', script_id: getScriptId() });
+  });
+
+  return { settings };
 });
