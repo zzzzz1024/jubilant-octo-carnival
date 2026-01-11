b8c783677013d4904616e4b8f4f57ee2121ad2dc
StageDog <aksanajw845@gmail.com>
2026-01-11 22:35:28 +0800

    fix: 调整 pnpm dump 对 windows 的兼容性

--- dump_schema.ts
+++ dump_schema.ts
@@ -9,7 +9,9 @@ fs.globSync('src/**/schema.ts').forEach(async schema_file => {
   try {
     globalThis._ = _;
     globalThis.z = z;
-    const module = await import(path.resolve(import.meta.dirname, schema_file));
+    const module = await import(
+      (process.platform === 'win32' ? 'file://' : '') + path.resolve(import.meta.dirname, schema_file)
+    );
     if (_.has(module, 'Schema')) {
       const schema = _.get(module, 'Schema');
       if (_.isFunction(schema)) {
