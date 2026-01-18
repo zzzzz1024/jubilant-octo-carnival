b2b95dbfe5b4da843a02f2fc4a03ecf7f8c42726
StageDog <aksanajw845@gmail.com>
2026-01-18 20:30:34 +0800

    feat: literalYamlify + parseString

--- util/common.ts
+++ util/common.ts
@@ -1,4 +1,6 @@
 import { compare } from 'compare-versions';
+import JSON5 from 'json5';
+import { jsonrepair } from 'jsonrepair';
 import { toDotPath } from 'zod/v4/core';
 
 export function assignInplace<T>(destination: T[], new_array: T[]): T[] {
@@ -74,4 +76,36 @@ export function prettifyErrorWithInput(error: z.ZodError) {
       return lines;
     })
     .join('\n');
+}
+
+export function literalYamlify(value: any) {
+  return YAML.stringify(value, { blockQuote: 'literal' });
+}
+
+export function parseString(content: string) {
+  let parsed: unknown;
+  try {
+    parsed = YAML.parseDocument(content, { merge: true }).toJS();
+  } catch (yaml_error) {
+    try {
+      // eslint-disable-next-line import-x/no-named-as-default-member
+      parsed = JSON5.parse(content);
+    } catch (json5_error) {
+      try {
+        parsed = JSON.parse(jsonrepair(content));
+      } catch (json_error) {
+        const toError = (error: unknown) => (error instanceof Error ? error.message : String(error));
+        throw new Error(
+          literalYamlify({
+            ['要解析的字符串不是有效的 YAML/JSON 格式']: {
+              字符串内容: content,
+              YAML错误信息: toError(yaml_error),
+              JSON5错误信息: toError(json5_error),
+              尝试修复JSON时的错误信息: toError(json_error),
+            },
+          }),
+        );
+      }
+    }
+  }
 }
