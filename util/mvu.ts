1f4c231b5cb52d7c2b7719d4fecef01a11ea7345
StageDog <aksanajw845@gmail.com>
2026-02-01 02:42:21 +0800

    feat: 轮询 mvu 变量只在检测到与代码现有发生变化时才存储

--- util/mvu.ts
+++ util/mvu.ts
@@ -36,9 +36,9 @@ export function defineMvuDataStore<T extends z.ZodObject>(
           ignoreUpdates(() => {
             data.value = result.data;
           });
-        }
-        if (!_.isEqual(stat_data, result.data)) {
-          updateVariablesWith(variables => _.set(variables, 'stat_data', result.data), variable_option);
+          if (!_.isEqual(stat_data, result.data)) {
+            updateVariablesWith(variables => _.set(variables, 'stat_data', result.data), variable_option);
+          }
         }
       }, 2000);
 
