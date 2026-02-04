c5c505a961be1ace9de59db30aac4e228dcc81b0
StageDog <aksanajw845@gmail.com>
2026-02-04 12:36:16 +0800

    fix: 流式楼层界面应该处理 more_messages_loaded

--- util/streaming.ts
+++ util/streaming.ts
@@ -209,7 +209,9 @@ export function mountStreamingMessages(
       renderOneMessage(message_id);
     }),
   );
-  scopedEventOn(tavern_events.MESSAGE_DELETED, () => setTimeout(errorCatched(renderAllMessage), 1000));
+  [tavern_events.MORE_MESSAGES_LOADED, tavern_events.MESSAGE_DELETED].forEach(event =>
+    scopedEventOn(event, () => setTimeout(errorCatched(renderAllMessage), 1000)),
+  );
   scopedEventOn(tavern_events.STREAM_TOKEN_RECEIVED, message => {
     renderOneMessage(Number($('#chat').children('.mes.last_mes').attr('mesid')), message);
   });
