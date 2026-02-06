c3861d613023b24ce4f6dd769b7c69061512e845
StageDog <aksanajw845@gmail.com>
2026-02-06 14:13:55 +0800

    refactor

--- webpack.config.ts
+++ webpack.config.ts
@@ -97,7 +97,7 @@ function watch_tavern_helper(compiler: webpack.Compiler) {
 
     compiler.hooks.done.tap('watch_tavern_helper', () => {
       console.info('\n\x1b[36m[tavern_helper]\x1b[0m 检测到完成编译, 推送更新事件...');
-      if (compiler.options.plugins.find(plugin => plugin instanceof HtmlWebpackPlugin)) {
+      if (compiler.options.plugins.some(plugin => plugin instanceof HtmlWebpackPlugin)) {
         io.emit('message_iframe_updated');
       } else {
         io.emit('script_iframe_updated');
