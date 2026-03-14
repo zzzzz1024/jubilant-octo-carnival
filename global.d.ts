a29fe33091e38cf7a506efd6e481bac35ed6d521
StageDog <aksanajw845@gmail.com>
2026-03-14 14:04:55 +0800

    feat

--- global.d.ts
+++ global.d.ts
@@ -6,6 +6,10 @@ declare module '*?url' {
   const content: string;
   export default content;
 }
+declare module '*.css' {
+  const content: unknown;
+  export default content;
+}
 declare module '*.html' {
   const content: string;
   export default content;
@@ -14,8 +18,8 @@ declare module '*.md' {
   const content: string;
   export default content;
 }
-declare module '*.css' {
-  const content: unknown;
+declare module '*.yaml' {
+  const content: any;
   export default content;
 }
 declare module '*.vue' {
@@ -34,5 +38,7 @@ declare namespace z {
 }
 
 declare module 'https://testingcf.jsdelivr.net/gh/StageDog/tavern_resource/dist/util/mvu_zod.js' {
-  export function registerMvuSchema(schema: z.ZodType<Record<string, any>> | (() => z.ZodType<Record<string, any>>)): void;
+  export function registerMvuSchema(
+    schema: z.ZodType<Record<string, any>> | (() => z.ZodType<Record<string, any>>),
+  ): void;
 }
