5abb6bf12850634001b9aaa681f8a14bf846f3a1
StageDog <aksanajw845@gmail.com>
2026-09-11 13:07:10 +0800

    fix: eslint配置因版本变化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,7 @@ export default [
       ...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
       ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
       'better-tailwindcss/enforce-consistent-line-wrapping': ['off', { preferSingleLine: true, printWidth: 120 }],
-      'better-tailwindcss/no-unregistered-classes': ['off', { ignore: ['fa-*'] }],
+      'better-tailwindcss/no-unknown-classes': ['off', { ignore: ['fa-*'] }],
     },
     settings: {
       'better-tailwindcss': {
